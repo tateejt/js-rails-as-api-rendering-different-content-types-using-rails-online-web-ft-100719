a01ebce564072ca0f8e3ed7fe7cc573e46d2293b
Emery Tate <tateejt@gmail.com>
2020-02-08 23:38:53 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,7 +2,6 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     render json: {
-      birds: @birds, messages: ['Hello Birds', 'Goodbye Birds']
-    }
+      birds: @birds, messages: ['Hello Birds', 'Goodbye Birds']}.to_json
   end
 end
